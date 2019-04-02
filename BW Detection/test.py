80228994dae3e4079c61d8bb88fbdbdc546ebb45
Victor <victor.badenas@gmail.com>
2019-04-03 00:01:21 +0200

    included Dockerfile and docker-compose

--- BW Detection/test.py
+++ BW Detection/test.py
@@ -4,10 +4,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 from scipy.io import wavfile
-from scipy.signal import hilbert
+from scipy.signal import hilbert, find_peaks, get_window
 
 from numpy import array, sign, zeros
 from scipy.interpolate import interp1d
+
+def normalize(sig:list, log=False):
+	return sig / max(sig) if not log else (sig - max(sig))
+
+def smooth_function(sig, window_type= 'triang', window_len = 50, mode = 'same'):
+    return np.convolve(sig, get_window(window_type, window_len), mode=mode)
+
+def get_peaks(sig:list, xticks:list):
+    """Returns the x,y values of the peaks in sig
+
+    Args:
+        sig: numpy.array of the signal of which to fing the peaks
+        xticks: numpy.array of the corresponding x axis values for sig
+
+    Returns:
+        yval: y values of the peaks
+        xval: x values of the peaks
+    """
+
+    if len(sig) != len(xticks):
+        raise ValueError("xticks and sig must have the same length")
+
+    peaks, _ = find_peaks(sig)
+
+    tuplelist = [(a, b) for a, b in zip(xticks[peaks], sig[peaks])]
+    tuplelist.sort(key=lambda x: x[1], reverse=True)
+
+    xval = [a for a, b in tuplelist]
+    yval = [b for a, b in tuplelist]
+
+    return xval, yval
 
 def findEnvelopes(s):
 	
@@ -54,70 +85,52 @@ def findEnvelopes(s):
 	#Return everything
 	return q_u,q_l
 
-def detectBW(directory,sf=1):
+def detectBW(directory,sf=2):
 
-	fid = open(os.path.join(directory, 'BW.html'), 'w')
-	fid.write("<!DOCTYPE html>\n")
-	fid.write("<html>\n")
-	fid.write("\t<head></head>\n")
-	fid.write("\t<body>\n")
-	fid.write("\t\t<table style=\"width:100%\">\n")
-	fid.write("\t\t<tr>\n")
-	fid.write("\t\t\t<td><b>Name</b></td>\n")
-	fid.write("\t\t\t<td><b>BW</b></td>\n")
-	fid.write("\t\t\t<td><b>SR</b></td>\n")
-	fid.write("\t\t</tr>\n")
-
-	numberofsounds = []
-	numberofsounds = np.sum([np.append(numberofsounds,1) for file in os.listdir(directory) if os.path.isfile(directory + "/" + file)])
+	numberoffiles = np.sum([1 for file in os.listdir(directory)])
 	# numberofsounds = np.sum(numberofsounds)
 
-	soundcount = 0
-	for file in os.listdir(directory):
+	for i,fname in enumerate(os.listdir(directory)):
 		relativeBW = -1
 		SR = -1
-		fname = file
-		file = directory + "/" + file
-		monoflag = False;
+		fname = os.path.join(directory,fname)
 
-		if(not os.path.isfile(file)):
+		_, extension = os.path.splitext(fname)
+		if extension != ".wav":
 			continue
 
-		try:
-			soundcount += 1
-			fs,x = wavfile.read(file, mmap=True)
-			samples = x.shape[0]
-			channels = x.shape[1]
-			monoflag = channels == 1
+		fs,x = wavfile.read(fname, mmap=True)
+		samples = x.shape[0]
+		channels = x.shape[1] if len(x.shape) > 1 else 1
 
-			if not monoflag:
-				x = (x[:,0] + x[:,1])/2
-
-		except:
-			continue
+		if channels != 1:
+			x = (x[:,0] + x[:,1])/2
 
 		N = int(pow(2, np.ceil(np.log2(len(x)))))
+
+		f = np.arange(int(N/2))/N*fs
 		x = np.floor(2*x)/2**15
 		x = np.append(x,np.zeros(N-len(x)))
 		X = np.fft.fft(x)/np.sum(x)
 		mX = 20*np.log10(np.abs(X))
+		
+		#mX = mX * np.log(np.arange(N))
+		mX = normalize(mX,log=True)
+		smX = mX
+		for _ in range(sf):
+			smX,_ = findEnvelopes(smX)
+		
+		#smX = smooth_function(smX, window_type= 'hann', window_len = 30)
+		#smX = normalize(smX,log=True)
+		#xpeaks, ypeaks = get_peaks(smX[:int(N/2)],f)
 
-		hemX,_ = findEnvelopes(mX)
-		hemX,_ = findEnvelopes(hemX)
-
-		f = np.arange(int(N/2))/N*fs
+		
 		plt.plot(f,mX[:int(N/2)])
-		plt.plot(f,hemX[:int(N/2)])
+		plt.plot(f,smX[:int(N/2)],color='r')
 		plt.savefig(fname+".png")
 		plt.clf()
 
-		print("Progress:" + "%.2f" % round(soundcount*100/numberofsounds,2) + " %")
-		
-	fid.write("\t\t</table>\n")
-	fid.write("\t</body>\n")
-	fid.write("</html>\n")
-
-	fid.close()
+		print("Progress:" + "%.2f" % round((i+1)*100/numberoffiles,2) + " %")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Calculates the effective BW of a file")
