71104e8e190e242f6925c331200f30a2796f597a
Victor Badenas Crespo <victor.badenas@gmail.com>
2019-07-30 22:52:57 +0200

    wip evaluation 110 sounds

--- Evaluation/evaluation.py
+++ Evaluation/evaluation.py
@@ -21,27 +21,33 @@ def main(datasetPath):
             line = "Filename,BitDepth,Bandwidth,lowSNR,Saturation,Hum,Clicks,Silence,FalseStereo,OutofPhase,NoiseBursts\n"
             f.write(line)
 
+    num = 0
     for filename in os.listdir(datasetPath):
         if os.path.splitext(filename)[-1] != ".wav":
             continue
         if filename in alreadyRunFiles:
-            print(filename + " has already been evaluated")
+            num+=1
+            print(str(num) + ": " + filename + " has already been evaluated")
             continue
         fullfilename = os.path.join(datasetPath, filename)
-        print("Now evaluating: ", filename)
+        num+=1
+        print(str(num) + ": " + "Now evaluating: ", filename)
         audio, SR, channels, _, br, _ = AudioLoader(filename=fullfilename)()
         if channels > 1:
             fig, ax = plt.subplots(channels + 1, 1)
             for i in range(channels):
                 ax[i].plot(audio[:i])
             monoAudio = np.sum(audio, axis=1)/channels
-            ax[-1].plot(abs(np.fft.fft(monoAudio)))
+            ax[-1].plot(np.arange(int(len(monoAudio)/2))*SR/len(monoAudio),
+                       20*np.log10(abs(np.fft.fft(monoAudio)[:int(len(monoAudio)/2)])))
+            ax[-1].set_xscale("log")
             plt.show(block=False)
         else:
             audio = audio[:,0]
             fig, ax = plt.subplots(2, 1)
             ax[0].plot(audio)
-            ax[1].plot(20*np.log10(abs(np.fft.fft(audio)[:int(len(audio)/2)])))
+            ax[1].plot(np.arange(int(len(audio)/2))*SR/len(audio), 20*np.log10(abs(np.fft.fft(audio)[:int(len(audio)/2)])))
+            ax[1].set_xscale("log")
             plt.show(block=False)
 
         wave_obj = sa.WaveObject.from_wave_file(fullfilename)
