92f368f15b7195119c9f08f713d311d31fbd03f2
Victor Badenas Crespo <victor.badenas@gmail.com>
2019-08-03 17:20:07 +0200

    added multithreading to optimise reproduction and wip 700 soounds

--- Evaluation/evaluation.py
+++ Evaluation/evaluation.py
@@ -1,6 +1,10 @@
 import os
 import numpy as np
+from threading import Thread
+import time
 from evalfuncs import *
+
+stopPlayback = False
 
 def main(datasetPath):
 
@@ -45,20 +49,20 @@ def main(datasetPath):
         else:
             audio = audio[:,0]
             fig, ax = plt.subplots(2, 1)
-            ax[0].plot(audio)
+            ax[0].plot(np.arange(len(audio))/SR, audio)
             ax[1].plot(np.arange(int(len(audio)/2))*SR/len(audio), 20*np.log10(abs(np.fft.fft(audio)[:int(len(audio)/2)])))
             ax[1].set_xscale("log")
+            ax[1].set_xticks([20,20000])
             plt.show(block=False)
 
-        wave_obj = sa.WaveObject.from_wave_file(fullfilename)
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
+        t = Thread(target=play_sound, args=(fullfilename,))
+        t.start()
         results = {
             "BitDepth": biteval(fullfilename) ,
             "Bandwidth": bandeval(fullfilename) ,
             "lowSNR": snreval(fullfilename) ,
             "Saturation": sateval(fullfilename) ,
-            "Hum": humeval(fullfilename) ,
+            "Hum": humeval(fullfilename) , 
             "Clicks": clickeval(fullfilename) ,
             "Silence": silenceeval(fullfilename) ,
             "FalseStereo": fseval(fullfilename) ,
@@ -70,6 +74,20 @@ def main(datasetPath):
             line = filename + "," + \
                 ",".join([str(v) for k, v in results.items()]) + "\n"
             f.write(line)
+        global stopPlayback
+        stopPlayback = True
+        t.join()
+
+def play_sound(filename):
+    global stopPlayback
+    stopPlayback = False
+    wave_obj = sa.WaveObject.from_wave_file(filename)
+    play_obj = wave_obj.play()
+    while(play_obj.is_playing()):
+        if stopPlayback:
+            play_obj.stop()
+            return
+    return
 
 if __name__=="__main__":
     datasetPath = "../Dataset/test"
